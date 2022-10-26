b301d9479ef2a0b3bfc04b271150d3614930dee3
Paweł Nowak <pawel.nowak.external@worldline.com>
2022-10-26 10:25:03 +0200

    Transformation User into rich domain model

--- src/AwesomeShop.Domain/Entities/User.cs
+++ src/AwesomeShop.Domain/Entities/User.cs
@@ -1,13 +1,40 @@
-﻿using OnboardingIntegrationExample.AwesomeShop.Domain.Primitives;
+﻿using FluentValidation;
+using OnboardingIntegrationExample.AwesomeShop.Domain.Primitives;
+using OnboardingIntegrationExample.AwesomeShop.Domain.Validators;
 
 namespace OnboardingIntegrationExample.AwesomeShop.Domain.Entities;
 
 public class User
 {
-    public UserId Id { get; set; }
-    public string Username { get; set; } = default!;
-    public string PasswordHash { get; set; } = default!;
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-    public string EmailAddress { get; set; } = default!;
+    public UserId Id { get; }
+    public string Username { get; } 
+    public string PasswordHash { get; } 
+    public string FirstName { get; } 
+    public string LastName { get; }
+    public string EmailAddress { get; } 
+
+    private User(string username, string passwordHash, string firstName, string lastName, string emailAddress)
+    {
+        Id = UserId.New();
+        Username = username;
+        PasswordHash = passwordHash;
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+    }
+
+    public static User Create(string username, string passwordHash, string firstName, string lastName,
+        string emailAddress)
+    {
+        var user = new User(username, passwordHash, firstName, lastName, emailAddress);
+        var validator = new UserValidator();
+        var result = validator.Validate(user);
+        
+        if (!result.IsValid)
+        {
+            throw new ValidationException(result.Errors);
+        }
+
+        return user;
+    }
 }
---
+++ src/AwesomeShop.Domain/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using OnboardingIntegrationExample.AwesomeShop.Domain.Entities;
+
+namespace OnboardingIntegrationExample.AwesomeShop.Domain.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(x => x.Username).NotEmpty();
+        RuleFor(x => x.PasswordHash).NotEmpty();
+        RuleFor(x => x.EmailAddress).NotEmpty();
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+    }
+}
