4df5a81342c8a032f0f9cd8b3c5915ce0ae1fb89
Paweł Nowak <pawel.nowak.external@worldline.com>
2022-10-26 11:20:44 +0200

    Adding missing dependencies to project and setting up mapper

---
+++ src/AwesomeShop.Application/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnboardingIntegrationExample.AwesomeShop.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        var config = TypeAdapterConfig.GlobalSettings;
+        config.Scan(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+    
+}
--- src/AwesomeShop.Application/Usings.cs
+++ src/AwesomeShop.Application/Usings.cs
@@ -1,2 +1,4 @@
 ﻿global using OnboardingIntegrationExample.AwesomeShop.Domain.Primitives.Results;
 global using OnboardingIntegrationExample.AwesomeShop.Application.Common.Messaging;
+global using Mapster;
+global using MapsterMapper;
